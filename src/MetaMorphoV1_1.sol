15e47d5a9cef174cb4a223d5311661406c7b3937
planD <43724111+wyf-ACCEPT@users.noreply.github.com>
2025-08-12 16:42:21 +0800

    Feat/metamorpho upgradeable (#3)
    * [feat] rewrite MetaMorpho to upgradeable contract
    
    * [feat] rewrite MetaMorphoV1_1Factory, use beacon proxy pattern to create new vault
    
    * [fix] fix for `setFee` when fee is zero
    
    * [dep] update dep (form-data)

--- src/MetaMorphoV1_1.sol
+++ src/MetaMorphoV1_1.sol
@@ -24,23 +24,30 @@ import {MarketParamsLib} from "../lib/morpho-blue/src/libraries/MarketParamsLib.
 import {IERC20Metadata} from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {MorphoBalancesLib} from "../lib/morpho-blue/src/libraries/periphery/MorphoBalancesLib.sol";
 
-import {Multicall} from "../lib/openzeppelin-contracts/contracts/utils/Multicall.sol";
-import {Ownable2Step, Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable2Step.sol";
-import {ERC20Permit} from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {MulticallUpgradeable} from "../lib/openzeppelin-upgradeable/contracts/utils/MulticallUpgradeable.sol";
+import {Ownable2StepUpgradeable} from "../lib/openzeppelin-upgradeable/contracts/access/Ownable2StepUpgradeable.sol";
+import {ERC20PermitUpgradeable} from
+    "../lib/openzeppelin-upgradeable/contracts/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import {
     IERC20,
     IERC4626,
-    ERC20,
-    ERC4626,
+    ERC20Upgradeable,
+    ERC4626Upgradeable,
     Math,
     SafeERC20
-} from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC4626.sol";
+} from "../lib/openzeppelin-upgradeable/contracts/token/ERC20/extensions/ERC4626Upgradeable.sol";
 
 /// @title MetaMorpho
 /// @author Morpho Labs
 /// @custom:contact security@morpho.org
 /// @notice ERC4626 compliant vault allowing users to deposit assets to Morpho.
-contract MetaMorphoV1_1 is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaMorphoV1_1StaticTyping {
+contract MetaMorphoV1_1 is
+    ERC4626Upgradeable,
+    ERC20PermitUpgradeable,
+    Ownable2StepUpgradeable,
+    MulticallUpgradeable,
+    IMetaMorphoV1_1StaticTyping
+{
     using Math for uint256;
     using UtilsLib for uint256;
     using SafeCast for uint256;
@@ -56,10 +63,10 @@ contract MetaMorphoV1_1 is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaM
     /* IMMUTABLES */
 
     /// @inheritdoc IMetaMorphoV1_1Base
-    IMorpho public immutable MORPHO;
+    IMorpho public MORPHO;
 
     /// @inheritdoc IMetaMorphoV1_1Base
-    uint8 public immutable DECIMALS_OFFSET;
+    uint8 public DECIMALS_OFFSET;
 
     /* STORAGE */
 
@@ -116,6 +123,18 @@ contract MetaMorphoV1_1 is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaM
 
     /* CONSTRUCTOR */
 
+    /// @dev Immigrated to upgradeable version.
+    constructor(
+        address, /* owner */
+        address, /* morpho */
+        uint256, /* initialTimelock */
+        address, /* _asset */
+        string memory, /* __name */
+        string memory /* __symbol */
+    ) {
+        _disableInitializers();
+    }
+
     /// @dev Initializes the contract.
     /// @param owner The owner of the contract.
     /// @param morpho The address of the Morpho contract.
@@ -125,14 +144,21 @@ contract MetaMorphoV1_1 is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaM
     /// @param __symbol The symbol of the vault.
     /// @dev We pass "" as name and symbol to the ERC20 because these are overriden in this contract.
     /// This means that the contract deviates slightly from the ERC2612 standard.
-    constructor(
+    function initialize(
         address owner,
         address morpho,
         uint256 initialTimelock,
         address _asset,
         string memory __name,
         string memory __symbol
-    ) ERC4626(IERC20(_asset)) ERC20Permit("") ERC20("", "") Ownable(owner) {
+    ) external initializer {
+        __ERC4626_init(IERC20(_asset));
+        __ERC20Permit_init("");
+        __ERC20_init("", "");
+        __Ownable_init(owner);
+        __Ownable2Step_init();
+        __Multicall_init();
+
         if (morpho == address(0)) revert ErrorsLib.ZeroAddress();
         if (initialTimelock != 0) _checkTimelockBounds(initialTimelock);
         _setTimelock(initialTimelock);
@@ -516,15 +542,15 @@ contract MetaMorphoV1_1 is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaM
     /* ERC4626 (PUBLIC) */
 
     /// @inheritdoc IERC20Metadata
-    function decimals() public view override(ERC20, ERC4626) returns (uint8) {
-        return ERC4626.decimals();
+    function decimals() public view override(ERC20Upgradeable, ERC4626Upgradeable) returns (uint8) {
+        return ERC4626Upgradeable.decimals();
     }
 
-    function name() public view override(IERC20Metadata, ERC20) returns (string memory) {
+    function name() public view override(IERC20Metadata, ERC20Upgradeable) returns (string memory) {
         return _name;
     }
 
-    function symbol() public view override(IERC20Metadata, ERC20) returns (string memory) {
+    function symbol() public view override(IERC20Metadata, ERC20Upgradeable) returns (string memory) {
         return _symbol;
     }
 
@@ -612,7 +638,7 @@ contract MetaMorphoV1_1 is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaM
 
     /* ERC4626 (INTERNAL) */
 
-    /// @inheritdoc ERC4626
+    /// @inheritdoc ERC4626Upgradeable
     function _decimalsOffset() internal view override returns (uint8) {
         return DECIMALS_OFFSET;
     }
@@ -649,7 +675,7 @@ contract MetaMorphoV1_1 is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaM
         }
     }
 
-    /// @inheritdoc ERC4626
+    /// @inheritdoc ERC4626Upgradeable
     /// @dev The accrual of performance fees is taken into account in the conversion.
     function _convertToShares(uint256 assets, Math.Rounding rounding) internal view override returns (uint256) {
         (uint256 feeShares, uint256 newTotalAssets,) = _accruedFeeAndAssets();
@@ -657,7 +683,7 @@ contract MetaMorphoV1_1 is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaM
         return _convertToSharesWithTotals(assets, totalSupply() + feeShares, newTotalAssets, rounding);
     }
 
-    /// @inheritdoc ERC4626
+    /// @inheritdoc ERC4626Upgradeable
     /// @dev The accrual of performance fees is taken into account in the conversion.
     function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view override returns (uint256) {
         (uint256 feeShares, uint256 newTotalAssets,) = _accruedFeeAndAssets();
@@ -687,7 +713,7 @@ contract MetaMorphoV1_1 is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaM
         return shares.mulDiv(newTotalAssets + 1, newTotalSupply + 10 ** _decimalsOffset(), rounding);
     }
 
-    /// @inheritdoc ERC4626
+    /// @inheritdoc ERC4626Upgradeable
     /// @dev Used in mint or deposit to deposit the underlying asset to Morpho markets.
     function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal override {
         super._deposit(caller, receiver, assets, shares);
@@ -699,7 +725,7 @@ contract MetaMorphoV1_1 is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaM
         _updateLastTotalAssets(lastTotalAssets + assets);
     }
 
-    /// @inheritdoc ERC4626
+    /// @inheritdoc ERC4626Upgradeable
     /// @dev Used in redeem or withdraw to withdraw the underlying asset from Morpho markets.
     /// @dev Depending on 3 cases, reverts when withdrawing "too much" with:
     /// 1. NotEnoughLiquidity when withdrawing more than available liquidity.
@@ -891,7 +917,7 @@ contract MetaMorphoV1_1 is ERC4626, ERC20Permit, Ownable2Step, Multicall, IMetaM
     ) internal view returns (uint256) {
         // Inside a flashloan callback, liquidity on Morpho Blue may be limited to the singleton's balance.
         uint256 availableLiquidity = UtilsLib.min(
-            totalSupplyAssets - totalBorrowAssets, ERC20(marketParams.loanToken).balanceOf(address(MORPHO))
+            totalSupplyAssets - totalBorrowAssets, ERC20Upgradeable(marketParams.loanToken).balanceOf(address(MORPHO))
         );
 
         return UtilsLib.min(supplyAssets, availableLiquidity);
